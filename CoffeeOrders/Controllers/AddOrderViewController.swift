5fe1b49ad601204e99cace4fb2f3a14da907806c
hustin-aquino <justinmosesbitancor@gmail.com>
2024-09-24 22:38:23 +0800

    Add classes AddOrderViewController and OrdersTableViewController and their respective UIs on Main StoryBoard

---
+++ CoffeeOrders/Controllers/AddOrderViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddOrderViewController.swift
+//  CoffeeOrders
+//
+//  Created by Justin Bitancor (MDI Novare) on 9/24/24.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+}
---
+++ CoffeeOrders/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OrdersTableViewController.swift
+//  CoffeeOrders
+//
+//  Created by Justin Bitancor (MDI Novare) on 9/24/24.
+//
+
+import Foundation
+import UIKit
+
+
+class OrdersTableViewController: UITableViewController {
+    
+}
