2a1eceb0cdf585bbd0a7b356878632b463ce3889
hustin-aquino <justinmosesbitancor@gmail.com>
2024-09-25 00:06:55 +0800

    Converted segmented control to dynamic version

--- CoffeeOrders/Controllers/AddOrderViewController.swift
+++ CoffeeOrders/Controllers/AddOrderViewController.swift
@@ -11,8 +11,25 @@ import UIKit
 class AddOrderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     private var vm = AddCoffeeOrderViewModel()
-    
     @IBOutlet weak var tableView: UITableView!
+    private var coffeeSizesSegmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.coffeeSizesSegmentedControl = UISegmentedControl(items: self.vm.sizes)
+        self.coffeeSizesSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(self.coffeeSizesSegmentedControl)
+        
+        self.coffeeSizesSegmentedControl.topAnchor.constraint(equalTo: self.tableView.bottomAnchor, constant: 20).isActive = true
+        self.coffeeSizesSegmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.vm.types.count
